f95651750d27a5e2616c475d00d0096d07748352
protodoatt <protodoatt@gmail.com>
2024-01-17 18:28:22 +0200

    loginpage progress

--- Atestat/Form1.cs
+++ Atestat/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,42 @@ namespace Atestat
 {
     public partial class Form1 : Form
     {
+        SqlConnection con;
+        String sqlpoint = Application.StartupPath.Remove(49)+"JocEducativ.mdf";
+        Inregistrare inregistrare;
+        
         public Form1()
         {
             InitializeComponent();
+            //MessageBox.Show(sqlpoint.ToString());
+            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="+sqlpoint+";Integrated Security=True;Connect Timeout=30");
+            inregistrare = new Inregistrare(con);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != string.Empty && textBox2.Text != string.Empty)
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Utilizatori WHERE EmailUtilizator ='" + textBox1.Text + "' AND Parola ='" + textBox2.Text + "'", con);
+                if (cmd.ExecuteReader().Read())
+                {
+                    MessageBox.Show("success");
+                }
+                else MessageBox.Show("error");
+                con.Close();
+            }
+            else MessageBox.Show("rubricile nu sunt completate");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            inregistrare.Show();
         }
     }
 }
---
+++ Atestat/Inregistrare.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atestat
+{
+    public partial class Inregistrare : Form
+    {
+        SqlConnection con;
+        public Inregistrare(SqlConnection con)
+        {
+            InitializeComponent();
+            textBox3.PasswordChar = '*';
+            this.con = con;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand sqlCommand = new SqlCommand("SELECT * FROM Utilizatori WHERE EmailUtilizator='"+textBox1.Text+"' OR NumeUtilizator ='"+textBox2.Text+"'");
+            sqlCommand.Connection = con;
+            con.Open();
+            if(sqlCommand.ExecuteReader().Read())
+            {
+                MessageBox.Show("Email sau Utilizator deja ocupat");
+            }
+            else
+            {
+                con.Close();
+                con.Open();
+
+                sqlCommand = new SqlCommand("INSERT INTO Utilizatori (EmailUtilizator, NumeUtilizator, Parola) VALUES (@email, @nume, @pass)");
+                sqlCommand.Parameters.AddWithValue("@email", textBox1.Text);
+                sqlCommand.Parameters.AddWithValue("@pass", textBox3.Text);
+                sqlCommand.Parameters.AddWithValue("@nume", textBox2.Text);
+                sqlCommand.Connection = con;
+                sqlCommand.ExecuteNonQuery();
+                MessageBox.Show("Account created!");
+                textBox1.Text = string.Empty; textBox3.Text = string.Empty; textBox2.Text = string.Empty;
+                this.Hide();
+            }
+            con.Close();
+        }
+    }
+}
