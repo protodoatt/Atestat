401ee9eac604e88ca554febb194652291d1faae6
protodoatt <protodoatt@gmail.com>
2024-01-18 11:59:52 +0200

    alegejoc

--- Atestat/Form1.cs
+++ Atestat/Form1.cs
@@ -45,6 +45,9 @@ namespace Atestat
                 if (cmd.ExecuteReader().Read())
                 {
                     MessageBox.Show("success");
+                    AlegeJoc joc1 = new AlegeJoc(textBox2.Text, textBox1.Text);
+                    joc1.Show();
+                    this.Hide();
                 }
                 else MessageBox.Show("error");
                 con.Close();
