4f345b203bba733002d33896d8c3e9394b39d396
protodoatt <protodoatt@gmail.com>
2024-01-18 09:27:15 +0200

    Revert "localcommit"
    This reverts commit 1111a1df372e771eb0d96bdfe68ebbaccfc72ec3.

--- Atestat/Form1.cs
+++ Atestat/Form1.cs
@@ -52,10 +52,5 @@ namespace Atestat
         {
             inregistrare.Show();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            
-        }
     }
 }
