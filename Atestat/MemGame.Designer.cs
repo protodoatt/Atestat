c1e16aeba849b593a5f5a7eedf8288c38861ad42
protodoatt <protodoatt@gmail.com>
2024-02-29 12:50:17 +0200

    bleah

--- Atestat/MemGame.Designer.cs
+++ Atestat/MemGame.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(863, 246);
             this.Name = "MemGame";
             this.Text = "MemGame";
             this.Load += new System.EventHandler(this.MemGame_Load);
--- Atestat/MemGame.cs
+++ Atestat/MemGame.cs
@@ -12,20 +12,36 @@ namespace Atestat
 {
     public partial class MemGame : Form
     {
+        string[] panou_address;
+        int n = 3;
+        PictureBox[] boxes;
         public MemGame()
         {
+            string[] file_names = { "avion.png", "bloc.png", "caine.jpg", "caprioara.jpg", "iepure.png", "leu.jpg", "lup.jpg", "vulpe.png" };
             InitializeComponent();
             String base_string = Application.StartupPath;
             base_string = base_string.Remove(base_string.Length - 9);
-            base_string += "\\Resurse\\Imagini\\";
+            base_string += "Resurse\\Imagini\\";
             MessageBox.Show(base_string);
             string[] panou_address = new string[8];
-            MessageBox.Show(panou_address[2]);
+            panou_address = Enumerable.Repeat(base_string, 8).ToArray();
+            for(int i = 0; i < panou_address.Length; i++)
+            {
+                panou_address[i] += file_names[i];
+            }
+            //MessageBox.Show(panou_address[2]);
+            this.panou_address = panou_address;
         }
-
         private void MemGame_Load(object sender, EventArgs e)
         {
-
+            boxes = new PictureBox[panou_address.Length];
+            for(int i = 0; i<n; i++)
+            {
+                boxes[i] = new PictureBox();
+                boxes[i].Location = new Point(30, i*50);
+                boxes[i].ImageLocation = panou_address[i];
+                boxes[i].Show();
+            }
         }
     }
 }
